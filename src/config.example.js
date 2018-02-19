134c3f71c7771413fea343b8bdd1fb3fb331f12d
Matt Johnson <mzjohnson@caleres.com>
2018-02-19 15:01:53 -0600

    added thunk

---
+++ src/config.example.js
@@ -0,0 +1,7 @@
+// Firebase config
+export const firebaseConfig = {
+    apiKey: '<your-api-key>',
+    authDomain: '<your-auth-domain>',
+    databaseURL: '<your-database-url>',
+    storageBucket: '<your-storage-bucket>'
+}
